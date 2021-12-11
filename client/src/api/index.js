16b90dd6de77987c405fb6e3844f8ba0b26d85a6
Kyle Friel <businessfriel@gmail.com>
2021-12-11 00:00:40 -0500

    Auth implemented.

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+let baseURL = "http://localhost:5000";
+
+const API = axios.create({ baseURL, withCredentials: true });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem("profile")) {
