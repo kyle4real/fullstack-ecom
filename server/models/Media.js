621b28f494d5e5c9e5310306168faa647a834dc0
Kyle Friel <businessfriel@gmail.com>
2021-12-13 02:38:01 -0500

    Seeder and productCreate configure to add media positions.

--- server/models/Media.js
+++ server/models/Media.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const MediaSchema = new mongoose.Schema({
     url: { type: String },
     public_id: { type: String },
+    position: { type: Number },
     product: { type: mongoose.Schema.ObjectId, ref: "Product", required: true },
 });
 
