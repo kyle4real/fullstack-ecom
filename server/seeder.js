3b46a2428455f1375780425f614d8dfbfd1f67f0
Kyle Friel <businessfriel@gmail.com>
2021-12-12 20:55:57 -0500

    Seeder for mongo data.

--- server/seeder.js
+++ server/seeder.js
@@ -7,3 +7,61 @@ import "colors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config({ path: "./config/config.env" });
+
+// Load Models
+import Product from "./models/Product.js";
+
+// Connect DB
+mongoose.connect(process.env.MONGO_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Read JSON files
+let products = JSON.parse(
+    fs.readFileSync(`${dirname(fileURLToPath(import.meta.url))}/data/products.json`, "utf-8")
+);
+
+const importData = async () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            await Product.create(products);
+
+            resolve();
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+const deleteData = async () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            await Product.deleteMany();
+
+            resolve();
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+if (process.argv[2] === "-i") {
+    try {
+        await importData();
+        console.log(`DATA IMPORTED`.green.inverse);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        process.exit();
+    }
+} else if (process.argv[2] === "-d") {
+    try {
+        await deleteData();
+        console.log(`DATA DELETED`.red.inverse);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        process.exit();
+    }
+}
