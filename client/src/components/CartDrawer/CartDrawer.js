993033ade9bd5009ec1275a011c3fc1d2c477948
Kyle Friel <businessfriel@gmail.com>
2021-12-28 00:15:04 -0500

    test commit

--- client/src/components/CartDrawer/CartDrawer.js
+++ client/src/components/CartDrawer/CartDrawer.js
@@ -80,6 +80,7 @@ const CartDrawer = () => {
                                 const url = hasMedia
                                     ? product.variant.media.url
                                     : product.image.url || null;
+                                console.log("test");
                                 const hasVariants = product.variant.sku !== null;
 
                                 return (
