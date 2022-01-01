d3c2aa4e6d16c58d4df12720e89c7b3c3c3106e1
Kyle Friel <businessfriel@gmail.com>
2021-12-31 21:00:41 -0500

    Footer.

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,8 +1,81 @@
 import React from "react";
-import { SFooter } from "./styles";
+import { Fragment } from "react";
+import { useSelector } from "react-redux";
+import { logoImg } from "../../assets";
+import { SFlexContainer } from "../UI/Containers/styles";
+import { SPage } from "../UI/PageLayout/styles";
+import {
+    SCopyright,
+    SFacebookIcon,
+    SFooter,
+    SGrid,
+    SIGIcon,
+    SList,
+    SListItem,
+    SListItemLink,
+    SListTitle,
+    SLogoContainer,
+    SLogoImage,
+    SSocialIcon,
+    SSocialIcons,
+    SSpaceBetween,
+    STwitterIcon,
+    SYoutubeIcon,
+} from "./styles";
 
 const Footer = () => {
-    return <SFooter>FOOTERRR</SFooter>;
+    const { collectionsTitles } = useSelector((state) => state.collections);
+
+    return (
+        <SFooter>
+            <SPage customSize={"xl"}>
+                <SSpaceBetween>
+                    <SGrid>
+                        <SList>
+                            <SListTitle>Shop</SListTitle>
+                            <SListItemLink to="/shop">All Products</SListItemLink>
+                            {collectionsTitles?.map(({ title, slug }, index) => (
+                                <Fragment key={index}>
+                                    <SListItemLink to={`/shop/collections/${slug}`}>
+                                        {title}
+                                    </SListItemLink>
+                                </Fragment>
+                            ))}
+                        </SList>
+                        <SList>
+                            <SListTitle>Pages</SListTitle>
+                            {[
+                                { label: "About Us", to: "/about-us" },
+                                { label: "Contact Us", to: "/contact" },
+                                { label: "Newsletter", to: "/newsletter" },
+                            ].map(({ label, to }, index) => (
+                                <Fragment key={index}>
+                                    <SListItemLink to={to}>{label}</SListItemLink>
+                                </Fragment>
+                            ))}
+                        </SList>
+                        <SList>
+                            <SListTitle>My Account</SListTitle>
+                            <SListItemLink>Login</SListItemLink>
+                            <SListItemLink>Register</SListItemLink>
+                        </SList>
+                    </SGrid>
+                    <SSocialIcons>
+                        {[<SIGIcon />, <SFacebookIcon />, <STwitterIcon />, <SYoutubeIcon />].map(
+                            (icon, index) => (
+                                <Fragment key={index}>
+                                    <SSocialIcon>{icon}</SSocialIcon>
+                                </Fragment>
+                            )
+                        )}
+                    </SSocialIcons>
+                </SSpaceBetween>
+            </SPage>
+            <SFlexContainer>
+                <SCopyright>© 2021, Astro Vinyl Art</SCopyright>
+            </SFlexContainer>
+        </SFooter>
+    );
 };
 
 export default Footer;
--- client/src/components/Footer/styles.js
+++ client/src/components/Footer/styles.js
@@ -1,8 +1,87 @@
-import styled from "styled-components";
+import { Link } from "react-router-dom";
+import styled, { css } from "styled-components";
 
-import { v } from "../../styles/variables";
+import { v, b } from "../../styles/variables";
+
+import {
+    AiFillTwitterCircle,
+    AiOutlineFacebook,
+    AiOutlineInstagram,
+    AiOutlineYoutube,
+} from "react-icons/ai";
 
 export const SFooter = styled.div`
     box-shadow: inset 0 1px ${({ theme }) => theme.bg3};
     background: ${({ theme }) => theme.bg};
+    padding: ${v.mdSpacing} 0;
+`;
+
+export const SGrid = styled.div`
+    width: fit-content;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: ${v.xxlSpacing};
+`;
+export const SList = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+export const SListTitle = styled.span`
+    display: block;
+    margin-bottom: ${v.mdSpacing};
+    font-weight: 500;
+`;
+export const SListItemLink = styled(Link)`
+    display: block;
+    text-decoration: none;
+    color: ${({ theme }) => theme.text};
+    :not(:last-of-type) {
+        margin-bottom: ${v.smSpacing};
+    }
+
+    :hover {
+        color: ${({ theme }) => theme.primary};
+    }
+`;
+
+export const SSpaceBetween = styled.div`
+    display: flex;
+    align-items: center;
+
+    @media ${b.md} {
+        justify-content: space-between;
+    }
+`;
+
+export const SSocialIcons = styled.div`
+    display: flex;
+`;
+
+export const SSocialIcon = styled.div`
+    :not(:last-of-type) {
+        margin-right: ${v.mdSpacing};
+    }
+`;
+
+export const iconStyles = css`
+    width: 24px;
+    height: auto;
+    display: block;
+`;
+export const SIGIcon = styled(AiOutlineInstagram)`
+    ${iconStyles}
+`;
+export const SFacebookIcon = styled(AiOutlineFacebook)`
+    ${iconStyles}
+`;
+export const STwitterIcon = styled(AiFillTwitterCircle)`
+    ${iconStyles}
+`;
+export const SYoutubeIcon = styled(AiOutlineYoutube)`
+    ${iconStyles}
+`;
+
+export const SCopyright = styled.span`
+    font-size: 14px;
+    color: ${({ theme }) => theme.text2};
 `;
